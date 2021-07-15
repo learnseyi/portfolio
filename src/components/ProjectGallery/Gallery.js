1997746018a78a9059a5096bd43a3f8fc77da71a
Seyi Agboola <learnseyi@gmail.com>
2021-07-15 02:40:11 -0500

    Added new projects to projects page

--- src/components/ProjectGallery/Gallery.js
+++ src/components/ProjectGallery/Gallery.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Container,Row,Col,Image,Button} from 'react-bootstrap';
-import slimbook from './Thumbnail/slimbook.PNG';
+import omnifoods from './Thumbnail/omnifoods.PNG';
 import fashioncode from './Thumbnail/fashioncode.PNG';
+import slimFinancial from './Thumbnail/slim-financial.PNG'
 import './Gallery.css';
 
 const Gallery = ()=>{
@@ -11,24 +12,34 @@ const Gallery = ()=>{
                 <h1>Project Gallery</h1>
             </div>
             <Row >
-                <Col >
+                <Col className="col col-sm-12 col-md-6">
                     <div >
-                    <a href='https://github.com/learnseyi' target='_blank' rel="noreferrer"><h4 >Facebook Clone</h4></a>
-                    <Image className='img-style' src={slimbook}  />
+                    <a href='https://omnifoods-1.netlify.app/' target='_blank' rel="noreferrer"><h4 >Omnifoods</h4></a>
+                    <Image className='img-style' src={omnifoods}  />
                     <div className='d-flex justify-content-center pt-3'>
                     <Button className='button'size='sm'>Technical information</Button>
                     </div>
                     </div>
                 </Col>
-                <Col>
-                <a href='https://learnseyi.github.io/fashion-code-v1/' target='_blank' rel="noreferrer"><h4 >Fashioncode</h4></a>
-                <div className='pt-5'>
+                <Col className="col col-sm-12 col-md-6">
+                <a href='https://fashioncode-v2.netlify.app/' target='_blank' rel="noreferrer"><h4 >Fashioncode</h4></a>
+                <div >
                 <Image className='img-style-2' src={fashioncode} />
                     <div className='d-flex justify-content-center  pt-3'>
                     <Button className='button'size='sm'>Technical information</Button>
                     </div>
                     </div>
                 </Col>
+                <Col className="col col-sm-12 col-md-6">
+                <a href='https://slimfinancial.netlify.app/' target='_blank' rel="noreferrer"><h4 >Slim Financial</h4></a>
+                <div >
+                <Image className='img-style' src={slimFinancial} />
+                    <div className='d-flex justify-content-center  pt-3'>
+                    <Button className='button'size='sm'>Technical information</Button>
+                    </div>
+                    </div>
+                </Col>
+                <col className="col col-sm-12 col-md-6"></col>
                 
             </Row>
         </Container>
